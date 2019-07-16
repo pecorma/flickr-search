f63f08a2ed55839426207153d0570a848094c560
Jake Pecoraro <mjpecora1@gmail.com>
2019-07-16 17:31:48 -0400

    fix multidex issue

--- app/src/main/java/com/mjpecora/application/flickrsearch/util/Constants.kt
+++ app/src/main/java/com/mjpecora/application/flickrsearch/util/Constants.kt
@@ -2,4 +2,4 @@ package com.mjpecora.application.flickrsearch.util
 
 const val API_KEY = "675894853ae8ec6c242fa4c077bcf4a0"
 const val BASE_URL = "https://api.flickr.com/services/rest/"
-const val PAGE_SIZE = 4
+const val PAGE_SIZE = 5
